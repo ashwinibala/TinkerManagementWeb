6b4854ff2f228cfb948816cbf1aab0561dda4c01
VasaviSundhar <vasavisundhar@gmail.com>
2023-06-07 17:35:54 -0400

    Basic Page Layout
    Header, navigation and footer created. Basic form template for Tinker Registration Created

---
+++ src/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./shared.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+        <p>&copy;2023 Solution Matrix</p>
+    </div>
+    
+  );
+};
