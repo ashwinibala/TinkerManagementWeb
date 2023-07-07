bca023d41ae93bf7ad7296d6265a1ee9dfd1f14b
VasaviSundhar <vasavisundhar@gmail.com>
2023-07-07 16:30:03 -0400

    Changes made in apiService
    Changes made in apiService

--- src/Services/apiService.jsx
+++ src/Services/apiService.jsx
@@ -2,29 +2,39 @@ import axios from 'axios';
 
 
 async function apiService(clientDetails)
-    {
-        
-    try
-        {
-         await axios.post("http://localhost:8080/client",
-        {
-        username: clientDetails.username,
-        password: clientDetails.password,
-        firstName: clientDetails.firstName,
-        lastName: clientDetails.lastName,
-        email: clientDetails.email,
-        phone: clientDetails.phone,
-        address: clientDetails.address,
-        categoryId: clientDetails.category
-        
-        });
-        
-    
-        }
-    catch(err)
-        {
-          
-        }
-    return false;
-   }
+{
+try
+{
+await fetch(`http://localhost:8080/client`, {
+method: 'POST',
+headers: {
+'Accept': 'application/json',
+'Content-Type': 'application/json',
+'Access-Control-Allow-Origin': 'http://localhost:3000'
+},
+body: JSON.stringify({
+username: clientDetails.username,
+password: clientDetails.password,
+firstname: clientDetails.firstName,
+lastname: clientDetails.lastName,
+email: clientDetails.email,
+phone: clientDetails.phone,
+address: clientDetails.address,
+categoryId: 1
+})
+})
+.then(response => response.json())
+.then(data => {
+// Handle the response data
+})
+.catch(error => {
+// Handle any errors
+});
+}
+catch(err)
+{
+
+}
+return false;
+}
 export default apiService;
